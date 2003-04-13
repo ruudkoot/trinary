10b421ad4460b672fe3a92defc6d1f55a7d88c4e
Ruud Koot <inbox@ruudkoot.nl>
2003-04-13 16:46:43 +0000

    no message

--- src/core/system.c
+++ src/core/system.c
@@ -1,5 +1,5 @@
 void discardable sysInit(void)
 {
-    asm ("outb %%al,%%dx"::"a" (0x16),"d" (0x43));     //MACRO!!!
-    asm ("outb %%al,%%dx"::"a" (119),"d" (0x40));     //MACRO!!!
+    asm ("outb %%al,%%dx"::"a" (0x26),"d" (0x43));     //MACRO!!!
+    asm ("outb %%al,%%dx"::"a" (15),"d" (0x40));     //MACRO!!!
 }
