f3033218f00378756a14b4ee3f0821f203f4b8bb
Ruud Koot <inbox@ruudkoot.nl>
2003-04-19 07:45:23 +0000

    Stated working on a C version.

--- src/init/init.s
+++ src/init/init.s
@@ -1,8 +1,23 @@
-[BITS 16]
-[ORG 0x0000]
+;******************************************************************************;
+;* System Initialization                                                      *;
+;* Copyright (c) 2003, Rudy Koot (Trinary Technologies)                       *;
+;*                                                                            *;
+;* This program is free software; you can redistribute it and/or modify       *;
+;* it under the terms of the GNU General Public License as published by       *;
+;* the Free Software Foundation; either version 2 of the License, or          *;
+;* (at your option) any later version.                                        *;
+;******************************************************************************;
+
+;******************************************************************************;
+;* This modules initialzes the Boot Strap Processor and some of the basic     *;
+;* system hardware.                                                           *;
+;******************************************************************************;
+
+bits 16
+global _enterPM
+section text class=code public align=16 use16
 
 _enterPM:
-;testing 123
 
 	mov ax, 0x9000
 	mov ds, ax
@@ -244,7 +259,7 @@ Aye:
 	mov esp, 0xFFFFFFFF
 	clc
 	cld
-	
+
 	;STUPID FUCKING ORG!!!! YOU RUINED AN HOUR OF MY LIFE!!!!
 	;REMEMBER: BIN/ELF = 8:C0000000 
       ;          PECOFF  = 8:C0001000
@@ -430,4 +445,6 @@ init8259:
 
 	ret
 
-times (9216-4096)-($-$$) db 0
+;times (9216-4096)-($-$$) db 0
+
+group init text
