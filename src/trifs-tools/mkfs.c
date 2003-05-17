24176982e492d74d7f1ecce047f2c7e45db8b034
Ruud Koot <inbox@ruudkoot.nl>
2003-05-17 09:45:07 +0000

    Trinary File System Support

--- src/trifs-tools/mkfs.c
+++ src/trifs-tools/mkfs.c
@@ -11,13 +11,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <memory.h>
 
+#include "config.h"
 #include "trifs.h"
 
 void help(void);
+int makefs(void);
 int parse(int argc, char* argv[]);
 
-int version = 0;
+int version;
+
+int config_blocks;
+int config_bandsize;
+
+typedef struct
+{
+    unt64 reserved[4];
+    string type[8];
+    unt64 id;
+    unt64 a;
+    unt64 blocksize;
+    unt64 blockcount;
+    unt64 bandsize;
+    unt64 e;
+    unt64 f;
+    unt64 g;
+    unt64 h;
+    unt64 i;
+    unt64 j;
+    unt64 k;
+    unt64 logsize;
+    unt64 m;
+    unt64 n;
+    unt64 o;
+    unt64 platform;
+    unt64 q;
+    unt64 rootdirectory;
+    unt64 s;
+    unt64 fstype;
+    unt64 uid;
+    unt64 version;
+    unt64 w;
+    unt64 x;
+    unt64 y;
+    unt64 z;
+} superblock;
 
 int main(int argc, char* argv[])
 {
@@ -27,15 +66,74 @@ int main(int argc, char* argv[])
         exit(0);
     }
 
-    parse(argc, argv);
+    config_blocks = atoi(argv[2]);
+    printf ("mkfs: blocks = %i\n", config_blocks);
+
+    config_bandsize = atoi(argv[3]);
+    printf("mkfs: bandsize = %i\n", config_bandsize);
+
+    blocksize = atoi(argv[4]);
+    printf("mkfs: blocksize = %i\n", blocksize);
 
     opendevice(argv[1]);
+
+    makefs();
+}
+
+int makefs(void)
+{
+    union
+    {
+        superblock* sb;
+        unsigned char* data;
+    } root;
+
+    unsigned char* t;
+    unsigned char* b;
+    unsigned char* n;
+
+    unsigned i;
+
+    blocksize = 512;
+
+    root.data = malloc(blocksize);
+    memset(root.data, 0, blocksize);
+    memcpy(root.sb->type, "RootNode", 8);
+    root.sb->blockcount = 2880;
+    root.sb->bandsize = 36;
+    root.sb->logsize = 1;
+    root.sb->platform = 0xFFFFFFFFFFFFFFFF;
+    root.sb->rootdirectory = 0xDEADBEEFC0DEBABE;
+    root.sb->fstype = 1;
+    root.sb->uid = 0xBEBADEC0EFBEADDE;
+    root.sb->version = 0x20030430;
+
+    t = malloc(blocksize);
+    memset(t, 0, blocksize);
+    
+    b = malloc(blocksize);
+    memset(b, 0, blocksize);
+    b[0] = 7;
+
+    n = malloc(blocksize * 33);
+    memset(n, 'A', blocksize * 33);
+
+    printf ("mkfs: Building Filesystem\n");
+
+    for (i = 0; i < config_blocks; i += config_bandsize)
+    {
+        printf("mkfs: Building band %i\n", i / config_bandsize);
+        root.sb->id = i / config_bandsize;
+        writeblock(i, 1, root.data);
+        writeblock(i+1, 1, t);
+        writeblock(i+2, 1, b);
+    }
+
+    return 0;
 }
 
 int parse(int argc, char* argv[])
 {
-    
-    
     return 1;
 }
 
--- src/trifs-tools/trifs.h
+++ src/trifs-tools/trifs.h
@@ -13,19 +13,28 @@ unsigned blocksize;
 
 int opendevice(char* name)
 {
-    device = fopen(name, "a+b");
+    device = fopen(name, "w+b");
 
     if (device == NULL)
     {
         printf("mkfs: Could not open \'%s\'\n", name);
         exit(1);
     }
+
+    return 0;
+}
+
+void closedevice(void)
+{
+    fclose(device);
 }
 
 int readblock(unsigned block, unsigned count, unsigned char* buffer)
 {
     fseek(device, block * blocksize, SEEK_SET);
     fread(buffer, blocksize, count, device);
+
+    return 0;
 }
 
 int writeblock(unsigned block, unsigned count, unsigned char* buffer)
@@ -33,4 +42,5 @@ int writeblock(unsigned block, unsigned count, unsigned char* buffer)
     fseek(device, block * blocksize, SEEK_SET);
     fwrite(buffer, blocksize, count, device);
     fflush(device);
+    return 0;
 }
