ad8824cad2df8ec569b9af9036a01a3fe45c59c6
Ruud Koot <inbox@ruudkoot.nl>
2003-05-24 10:01:24 +0000

    I'm writing a real bootloader.

--- src/trifs-tools/trifs.h
+++ src/trifs-tools/trifs.h
@@ -11,7 +11,42 @@
 FILE* device;
 unsigned blocksize;
 
-int opendevice(char* name)
+typedef struct
+{
+    unt64 reserved[4];
+    string type[8];
+    unt64 id;
+    unt64 a;
+    unt64 blocksize;
+    unt64 blockcount;
+    unt64 bandsize;
+    unt64 e;
+    unt64 f;
+    unt64 g;
+    unt64 h;
+    unt64 initfilenode;
+    unt64 j;
+    unt64 k;
+    unt64 logsize;
+    unt64 m;
+    unt64 n;
+    unt64 o;
+    unt64 platform;
+    unt64 q;
+    unt64 rootdirectory;
+    unt64 s;
+    unt64 fstype;
+    unt64 uid;
+    unt64 version;
+    unt64 w;
+    unt64 x;
+    unt64 y;
+    unt64 z;
+} superblock;
+
+int readblock(unsigned block, unsigned count, unsigned char* buffer);
+
+int createdevice(char* name)
 {
     device = fopen(name, "w+b");
 
@@ -20,6 +55,32 @@ int opendevice(char* name)
         printf("mkfs: Could not open \'%s\'\n", name);
         exit(1);
     }
+
+    return 0;
+}
+
+int opendevice(char* name)
+{
+    union
+    {
+        unsigned char* data;
+        superblock* sb;
+    } root;
+    
+    device = fopen(name, "r+b");
+
+    if (device == NULL)
+    {
+        printf("mkfs: Could not open \'%s\'\n", name);
+        exit(1);
+    }
+
+    blocksize = 256;
+    root.data = malloc(blocksize);
+    readblock(0, 1, root.data);
+
+    blocksize = root.sb->blocksize;
+    free(root.data);
 
     return 0;
 }
@@ -33,6 +94,7 @@ int readblock(unsigned block, unsigned count, unsigned char* buffer)
 {
     fseek(device, block * blocksize, SEEK_SET);
     fread(buffer, blocksize, count, device);
+    fflush(device);
 
     return 0;
 }
