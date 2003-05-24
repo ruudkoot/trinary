fe90bdcd777e9223a4b8ec529866aae42d09fe69
Ruud Koot <inbox@ruudkoot.nl>
2003-05-24 10:14:10 +0000

    no message

---
+++ src/boot/arch/ia32/ibm/trifs.s
@@ -0,0 +1,261 @@
+/******************************************************************************/
+/* Boot Sector for TriFS Disks                                                */
+/* Copyright (c) 2003, Rudy Koot (Trinary Technologies)                       */
+/*                                                                            */
+/* This program is free software; you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation; either version 2 of the License, or          */
+/* (at your option) any later version.                                        */
+/******************************************************************************/
+
+.arch i8086, jumps;
+.code16;
+.text;
+
+/******************************************************************************/
+/* This function is the entry point of the boot sector. The first thing we do */
+/* is setting up the segments and stack frame. We only have 24 bytes, before  */
+/* we have to jump to next section that is reserved for the boot code.        */
+/******************************************************************************/
+boot_entry:
+
+    cli;
+    xorw %ax, %ax;
+    movw %ax, %ds;
+    movw %ax, %es;
+    movw %ax, %ss;
+    movw $0x7C00, %sp;
+    ljmp $0, $start;
+
+boot_setcursorposition:
+	movb $0x02, %ah
+	movb $0x00, %bh
+	int $0x10
+	ret
+
+    /* Pad the unused space with 0's or give an error message if we've        *
+     * written to much code.                                                  */
+    . = 24;
+
+/******************************************************************************/
+/* This section defines the physical disk geometry structure. It might be     */
+/* nicer to use a BIOS call or probing to automagically detect these values,  */
+/* but it's unlikely we have enough space in the boot sector to do that.      */
+/******************************************************************************/
+cylinders:      .word 80;
+heads:          .word 2;
+sectors:        .word 18;
+sectorsize:     .word 512;
+
+/******************************************************************************/
+/* This section defines the Root Node.                                        */
+/******************************************************************************/
+type:           .quad 0;
+id:             .quad 0;
+a:              .quad 0;
+blocksize:      .quad 0;
+blockcount:     .quad 0;
+bandsize:       .quad 0;
+e:              .quad 0;
+f:              .quad 0;
+g:              .quad 0;
+h:              .quad 0;
+initfilenode:   .quad 0;
+j:              .quad 0;
+k:              .quad 0;
+logsize:        .quad 0;
+m:              .quad 0;
+n:              .quad 0;
+o:              .quad 0;
+platform:       .quad 0;
+q:              .quad 0;
+rootdirectory:  .quad 0;
+s:              .quad 0;
+fstype:         .quad 0;
+uid:            .quad 0;
+version:        .quad 0;
+w:              .quad 0;
+x:              .quad 0;
+y:              .quad 0;
+z:              .quad 0;
+
+boot_title:     .asciz "Trinary Operating System"
+
+boot_writestring:
+	cld;
+	1:
+                lodsb;
+                orb %al, %al;
+                jz 1f;
+                movb $0x0E, %ah;
+                movw $0x000F, %bx;
+                int $0x10;
+                jmp 1b;
+	1:
+                ret;
+
+/******************************************************************************/
+/* This function locates Init on disk, loads it into mememory and executes    */
+/* it.                                                                        */
+/******************************************************************************/
+start:
+
+    /* Because this code will only be used for floppy disk we don't use the   *
+     * interrupt 13 extensions, and will there fore be limited to disk sizes  *
+     * of 528 MB or 8 GB (depending wheter or not the BIOS supports and uses  *
+     * translation), which sould obviously be enough for floppy disk. This    *
+     * should be changed, because this code can then also be used for         *
+     * dedicated (unpartitioned) disks.                                       *
+     *                                                                        *
+     * Because we use 16 bit code and 32 bit divides and multiplies, the      *
+     * maximum number of blocks is limited to 4 billion, which should still   *
+     * be enough to support disks up to 2 TB. I want to add check, that       *
+     * blocks aren't located above that limit.                                */
+	
+    /* Detect the type of videocard, so we can use the right color sheme.     */
+    int $0x11;
+	testb $0x30, %al;
+	movw $0x0070, %bx
+	movw $0x0007, %ax;
+	je 1f;
+	movb $0x1f, %bl
+	movb $0x03, %al;
+    1:
+	int $0x10;
+
+	/* Hide the cursor.                                                       */
+	movw $0x2000, %cx;
+	movb $0x01, %ah;
+	int $0x10;
+
+    /* Draw the titlebar.                                                     */
+	movw $0x0800, %cx;
+	movw $0x0900, %ax;
+	int $0x10;
+
+	movw $0x0600, %ax;
+	movb $0x0F, %bh;
+	movw $0x0102, %cx;
+	movw $0x184F, %dx;
+	int $0x10;
+
+    /* Draw the caption.                                                      */
+    movw $0x001C, %dx;
+	call boot_setcursorposition;
+	mov $boot_title, %si;
+	call boot_writestring;
+
+    /* Load the file node of Init into memory. The block of the node is       *
+     * stored in the superblock. We can do this, because file nodes, usualy   *
+     * don't move around. This has two advantage. We don't have to parse      *
+     * large parts of the filesystem (at least the root directory, maybe even *
+     * more directories, which is quite complex, because we have to take      *
+     * heavy fragmentation into account, and parse the hashed and regular     *
+     * filenames), which requires too much code and is quite slow, because of *
+     * excesive disk access. An additional advantage is the fact that we can  *
+     * choose any file to boot from.                                          *
+     *                                                                        *     
+     * The size of the file node is limited to 32 kB, although it can be      *
+     * extended to 64 kB or even more.                                        *
+     *                                                                        */
+
+    movw initfilenode, %si;
+    movw initfilenode+2, %di;
+    movw $0x8000, %bx;
+    call loadblock;
+
+    movw 0x8118, %si;
+    movw 0x811A, %di;
+    movw 0x8120, %cx;
+    movw $0x9000, %ax;
+    movw %ax, %es;
+    xorw %bx, %bx;
+
+    myloadloop:
+        pushw %cx;
+        pushw %si;
+        pushw %di;
+        pushw %bx;
+        call loadblock;
+        popw %bx;
+        popw %di;
+        popw %si;
+        addw $512, %bx;
+        addw $1, %si;
+        adcw $0, %di;
+        popw %cx;
+    loop myloadloop;
+
+    movw $0x9000, %ax;
+    movw %ax, %ds;
+    movw %ax, %ss;
+    movw $0, %sp;
+    ljmp $0x9000, $0
+
+/******************************************************************************/
+/* loadblock - Load Block into Memory                                         */
+/*                                                                            */
+/* This function loads a block into memory independant of the disk's geometry */
+/* or sector size.                                                            */
+/*                                                                            */
+/* THIS FUNCTION ASSUMES (AT THE MOMENT) THAT BOTH THE SECTOR AND THE BLOCK   */
+/* SIZE ARE 512 BYTES!                                                        */
+/******************************************************************************/
+loadblock:
+
+    /* Calculate the number of sectors per block. If DX doesn't equal 0 eiter *
+     * the sector size or the block size was invalid. We do not check for     *
+     * this condition at the moment.                                          */
+    movw blocksize, %ax;
+    divw sectorsize;
+
+    call loadsector;
+
+
+    ret;
+
+/******************************************************************************/
+/* loadsector - Load Sector into Memory                                       */
+/*                                                                            */
+/* %di:%si: sector                                                            */
+/* %bx: address to load                                                       */
+/*                                                                            */
+/* Destroys ALL registers (except SP)                                         */
+/******************************************************************************/
+loadsector:
+
+    /* Convert the sector number to a CHS address. We use the formula         *
+     *     C = B / (Nh * Ns)                                                  *
+     *     t = B % (Nh * Ns)                                                  *
+     *     H = t / Ns                                                         *
+     *     S = (t % Ns) + 1                                                   */
+    movw sectors, %ax;
+    mulw heads;
+    movw %ax, %bp;
+    movw %si, %ax;
+    movw %di, %dx;
+    divw %bp;
+    movb %al, %ch;
+    movw %dx, %ax;
+    xorw %dx, %dx;
+    divw sectors;
+    incw %dx
+    movb %dl, %cl;
+    movb %al, %dh;
+
+    /* The drive to load the sectors from!                                    */
+    xorb %dl, %dl;
+
+    mov $0x0201, %ax;
+    int $0x13;
+
+    ret;
+
+
+
+
+
+
+    . = 510;
+    .byte 0x55;
+    .byte 0xAA;
---
+++ src/core/arch/ia32/ibm/system.c
@@ -0,0 +1,147 @@
+/******************************************************************************/
+/* System Hardware Initialization                                             */
+/* Copyright (c) 2003, Rudy Koot (Trinary Technologies)                       */
+/*                                                                            */
+/* This program is free software; you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation; either version 2 of the License, or          */
+/* (at your option) any later version.                                        */
+/******************************************************************************/
+
+void sys_arch_init(void);
+unt8 sys_arch_ioread8(unt16 port);
+unt16 sys_arch_ioread16(unt16 port);
+unt32 sys_arch_ioread32(unt16 port);
+void sys_arch_iowrite8(unt16 port, unt8 value);
+void sys_arch_iowrite16(unt16 port, unt16 value);
+void sys_arch_iowrite32(unt16 port, unt32 value);
+void sys_arch_timer_init(void);
+void sys_arch_timer_setfrequency(unt8 timer, unt32 frequency);
+
+/******************************************************************************/
+/* sys_arch_init - Initialize System Hardware                                 */
+/*                                                                            */
+/* This function initializes some of the system hardware that has to be       */
+/* managed by the micro-kernel itself (interval timer, interrupt controller). */
+/******************************************************************************/
+void sys_arch_init(void)
+{
+    sys_arch_timer_init();
+}
+
+/******************************************************************************/
+/* sys_arch_ioread8 - Read Byte from I/O Port                                 */
+/*                                                                            */
+/* This function reads a byte from one of the processor's I/O ports.          */
+/******************************************************************************/
+unt8 sys_arch_ioread8(unt16 port)
+{
+    unt8 value;
+    
+    asm ("inb %%dx, %%al" : "=a" (value) : "d" (port));
+
+    return value;
+}
+
+/******************************************************************************/
+/* sys_arch_ioread16 - Read Word from I/O Port                                */
+/*                                                                            */
+/* This function reads a word from one of the processor's I/O ports.          */
+/******************************************************************************/
+unt16 sys_arch_ioread16(unt16 port)
+{
+    unt16 value;
+    
+    asm ("inw %%dx, %%ax" : "=a" (value) : "d" (port));
+
+    return value;
+}
+
+/******************************************************************************/
+/* sys_arch_ioread32 - Read Double Word from I/O Port                         */
+/*                                                                            */
+/* This function reads a double word from one of the processor's I/O ports.   */
+/******************************************************************************/
+unt32 sys_arch_ioread32(unt16 port)
+{
+    unt32 value;
+    
+    asm ("inl %%dx, %%eax" : "=a" (value) : "d" (port));
+
+    return value;
+}
+
+/******************************************************************************/
+/* sys_arch_iowrite8 - Write Byte to I/O Port                                 */
+/*                                                                            */
+/* This function writes a byte to one of the processor's I/O ports.           */
+/******************************************************************************/
+void sys_arch_iowrite8(unt16 port, unt8 value)
+{
+    asm ("outb %%al, %%dx" : : "a" (value), "d" (port));
+}
+
+/******************************************************************************/
+/* sys_arch_iowrite16 - Write Word to I/O Port                                */
+/*                                                                            */
+/* This function writes a word to one of the processor's I/O ports.           */
+/******************************************************************************/
+void sys_arch_iowrite16(unt16 port, unt16 value)
+{
+    asm ("outw %%ax, %%dx" : : "a" (value), "d" (port));
+}
+
+/******************************************************************************/
+/* sys_arch_iowrite32 - Write Double Word to I/O Port                         */
+/*                                                                            */
+/* This function writes a double word to one of the processor's I/O ports.    */
+/******************************************************************************/
+void sys_arch_iowrite32(unt16 port, unt32 value)
+{
+    asm ("outl %%eax, %%dx" : : "a" (value), "d" (port));
+}
+
+/******************************************************************************/
+/* sys_arch_timer_init - Initialize the Programmable Interval Timer (PIT)     */
+/*                                                                            */
+/* This function initializes the Intel 8254 PIT, which is present on all IBM  */
+/* compatible machines.                                                       */
+/*                                                                            */
+/* Note: The 8254 PIT might not be available on all IA-32 machines. We should */
+/* probably make a hierachical achitecture system that could look something   */
+/* like this: Trinary => IA-32 => IBM.                                        */
+/******************************************************************************/
+void sys_arch_timer_init(void)
+{
+    sys_arch_timer_setfrequency(0, 2000);
+}
+
+/******************************************************************************/
+/* sys_arch_timer_setfrequency - Set Timer Frequency                          */
+/*                                                                            */
+/* This function set the frequency of the PIT. It assumes a base clock of     */
+/* 1.193181 MHz.                                                              */
+/******************************************************************************/
+void sys_arch_timer_setfrequency(unt8 timer, unt32 frequency)
+{
+    unt16 divider;
+
+    if (frequency < 19) panic("PIT Frequency too small!");
+
+    /* Note that my frequency differs from some standard frequencies you      *
+     * find in other programs. Many use 1193180, because some of the standard *
+     * works say the freqeuncy is 1.19318 MHz, this is a rounding error. The  *
+     * true frequency is 1.193181666... Mhz. I've rounded it down instead of  *
+     * up, because a higher frequency causes less noticable error that a      *
+     * lower frequency.                                                       */
+    divider = 1193181 / frequency;
+
+    logDec("PIT Frequency", frequency);
+    logDec("PIT Divider", divider);
+
+    sys_arch_iowrite8(0x61, 0x03);
+
+    sys_arch_iowrite8(0x43, 0xB6);
+    sys_arch_iowrite8(0x42, divider & 0xFF);
+    sys_arch_iowrite8(0x42, divider >> 8);
+}
