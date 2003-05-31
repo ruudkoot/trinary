aac5dd9e5684a324a99e75a8c7da93982aba1d30
Ruud Koot <inbox@ruudkoot.nl>
2003-05-31 10:28:10 +0000

    no message

---
+++ src/init/fat.c
@@ -0,0 +1,9 @@
+/******************************************************************************/
+/* System Initialization : File System : FAT12 / FAT16 / FAT32                */
+/* Copyright (c) 2003, Rudy Koot (Trinary Technologies)                       */
+/*                                                                            */
+/* This program is free software; you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation; either version 2 of the License, or          */
+/* (at your option) any later version.                                        */
+/******************************************************************************/
