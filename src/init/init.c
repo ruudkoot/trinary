19cfb044366d209a2b999b325768726e128947e4
Ruud Koot <inbox@ruudkoot.nl>
2003-05-31 11:01:24 +0000

    no message

--- src/init/init.c
+++ src/init/init.c
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* System Initialization : File System : Ext2 FS                              */
+/* System Initialization                                                      */
 /* Copyright (c) 2003, Rudy Koot (Trinary Technologies)                       */
 /*                                                                            */
 /* This program is free software; you can redistribute it and/or modify       */
@@ -8,6 +8,28 @@
 /* (at your option) any later version.                                        */
 /******************************************************************************/
 
-void ext2_init(void)
+#include "../lib/config.c"
+#include "../lib/lib.c"
+
+#include "repository.c"
+
+#include "ia32/ibm/log.c"
+#include "ia32/ibm/disk.c"
+
+#include "ibm.c"
+#include "trifs.c"
+#include "fat.c"
+#include "ext2.c"
+
+void entry(void)
 {
+    log_item("Initializing Repository");
+    log_status(log_status_success);
+    log_subitem("Trinary File System");
+    log_subitem("FAT12 / FAT16 / FAT32");
+    log_subitem("Ext2 FS");
+
+    disk_init();
+    
+    return;
 }
