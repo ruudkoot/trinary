9da03b947210c778f41409c89d9efbf57aca623c
Ruud Koot <inbox@ruudkoot.nl>
2003-06-07 09:45:48 +0000

    no message

---
+++ src/trifs-tools/fat2trifs.c
@@ -0,0 +1,22 @@
+/******************************************************************************/
+/* Trinary File System Tools : Convert FAT to TriFS                           */
+/* Copyright (c) 2003, Rudy Koot (Trinary Technologies)                       */
+/*                                                                            */
+/* This program is free software; you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation; either version 2 of the License, or          */
+/* (at your option) any later version.                                        */
+/******************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <memory.h>
+
+#include "config.h"
+#include "trifs.h"
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
