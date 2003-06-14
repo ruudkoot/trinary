358a6667751112911315e6a5a3a62aadf9e788e2
Ruud Koot <inbox@ruudkoot.nl>
2003-06-14 14:16:36 +0000

    no message

---
+++ src/lib/bitmap/bitset.c
@@ -0,0 +1,29 @@
+/******************************************************************************/
+/*                                                                            */
+/* Copyright (c) 2003, Rudy Koot (Trinary Technologies)                       */
+/*                                                                            */
+/* This program is free software; you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation; either version 2 of the License, or          */
+/* (at your option) any later version.                                        */
+/******************************************************************************/
+
+void bitput(void* bitmap, unt32 bit)
+{
+
+}
+
+void bitset(void* bitmap, unt32 bit)
+{
+    ((unt32*)(bitmap))[bit / 32] |= (1 << (bit % 32));
+}
+
+void bitclr(void* bitmap, unt32 bit)
+{
+    ((unt32*)(bitmap))[bit / 32] |= (1 << (bit % 32));
+}
+
+void bitinv(void* bitmap, unt32 bit)
+{
+    ((unt32*)(bitmap))[bit / 32] |= (1 << (bit % 32));
+}
