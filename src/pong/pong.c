376d0f9a1265c2e152b8b5dc186c2d8f9b6ab876
Ruud Koot <inbox@ruudkoot.nl>
2003-10-10 00:01:59 +0000

    no message

---
+++ src/pong/pong.c
@@ -0,0 +1,38 @@
+/******************************************************************************/
+/* Pong Application                                                           */
+/* Copyright (c) 2003, Rudy Koot (Trinary Technologies)                       */
+/*                                                                            */
+/* This program is free software; you can redistribute it and/or modify       */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation; either version 2 of the License, or          */
+/* (at your option) any later version.                                        */
+/******************************************************************************/
+
+#define NOMEM
+#include "../lib/config.c"
+#include "../lib/lib.c"
+
+void out(char* output)
+{
+    int i;
+    char* m = (char*)0xB80C8;
+
+    while (*output)
+    {
+        *m = *output;
+        m++;
+        *m = 0x07;
+        m++;
+        output++;
+    }
+}
+
+void cmain(void)
+{
+    unsigned i;
+    
+    for (i = 0;; i++)
+	{
+		out("Pong");
+	}
+}
