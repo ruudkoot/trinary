57e35d972ff20a79516ee58515a0b70f7dcf5ac3
Ruud Koot <inbox@ruudkoot.nl>
2003-12-18 00:47:30 +0000

    *** empty log message ***

--- src/idl/idl.c
+++ src/idl/idl.c
@@ -7,3 +7,8 @@
 /* the Free Software Foundation; either version 2 of the License, or          */
 /* (at your option) any later version.                                        */
 /******************************************************************************/
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
